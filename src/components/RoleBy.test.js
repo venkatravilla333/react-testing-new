fce7b8134d151568817f1995eca600d401a3ec6b
venkat <venkatravilla999@gmail.com>
2024-09-27 09:39:50 +0530

    new test

--- src/components/RoleBy.test.js
+++ src/components/RoleBy.test.js
@@ -1,10 +1,17 @@
 import { render, screen } from "@testing-library/react"
 import RoleBy from "./RoleBy"
 
-test('check render login button', () => {
+test('check login button', () => {
   render(<RoleBy />)
  var loginBtn = screen.getByRole('button', {
     name: 'Login'
+  })
+  expect(loginBtn).toBeInTheDocument()
+})
+test('check logout button', () => {
+  render(<RoleBy />)
+ var loginBtn = screen.getByRole('button', {
+    name: 'Logout'
   })
   expect(loginBtn).toBeInTheDocument()
 })
